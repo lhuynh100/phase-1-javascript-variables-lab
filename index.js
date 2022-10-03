fa55bdf534d78bfc21c3451293824e01d13399e3
Le Huynh <lhuynh100@gmail.com>
2022-10-03 14:01:35 -0500

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber';
+{
+    const companyName = companyName;
+}
+
+const mostProfitableNeighborhood = 'Chelsea';
+{
+    let mostProfitableNeighborhood = 'Chelsea';
+}
+
+const companyCeo = 'Susan Smith'; {
+    let companyCeo = 'Susan Smith';
+} 
